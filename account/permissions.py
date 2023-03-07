b19358f1e26c055728a61662b91da9951f2ce9d4
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2023-03-07 18:53:16 +0300

    account CRUD

---
+++ account/permissions.py
@@ -0,0 +1,18 @@
+
+from rest_framework import permissions
+from django.conf import settings
+
+
+class UserTypeAccessAdminOrClient(permissions.BasePermission):
+
+    """Permissions (USER either ADMIN or CLIENT)"""
+
+
+    ### USER either ADMIN OR CLIENT ###
+    message = 'Account type is not permitted to preform this request'
+
+    def has_permission(self, request, view):
+        if request.user.user_type != settings.SCANNER:
+             return True
+
+        return False
